bf56b85a8362367b7f681ed5d9c87076e39becf8
Diogo Lundberg <dclundberg@gmail.com>
2022-04-16 16:17:28 +0200

    Add webmvc with embedded jetty

--- src/main/java/webmvc/Config.java
+++ src/main/java/webmvc/Config.java
@@ -1,7 +1,9 @@
 package webmvc;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("webmvc")
 public class Config {
 }
