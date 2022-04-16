0db9964867ad127a640d1b46126741d397a1af3c
Diogo Lundberg <dclundberg@gmail.com>
2022-04-16 16:17:28 +0200

    Add webmvc with embedded jetty

--- src/main/java/webmvc/Application.java
+++ src/main/java/webmvc/Application.java
@@ -1,14 +1,22 @@
 package webmvc;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import webmvc.service.CrudService;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class Application {
-    public static void main(String[] params) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        var service = context.getBean(CrudService.class);
-        System.out.println(service.list());
+    public static void main(String[] params) throws Exception {
+        AnnotationConfigWebApplicationContext webApplication = new AnnotationConfigWebApplicationContext();
+        webApplication.register(Config.class);
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplication);
 
+        ServletContextHandler contextHandler = new ServletContextHandler();
+        contextHandler.addServlet(new ServletHolder(dispatcherServlet), "/");
+
+        Server server = new Server(8080);
+        server.setHandler(contextHandler);
+        server.start();
     }
 }
